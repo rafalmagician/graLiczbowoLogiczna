d2f9bb4b1d86c395e9a12e51b17b535c6b6efd0e
Rafal Walasek <rafal_walasek@interia.pl>
2018-04-09 10:45:21 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,14 +2,30 @@
 #include <time.h>
 #include <cstdlib>
 using namespace std;
-#include "class.h"
+#include "game.h"
+#include "menu.h"
 
 int main()
 {
     srand(time(0));
 
+    int x;
+    Menu m1;
     Game g1;
-        g1._game();
 
+        do{
+            m1._menu();
+            cout<<endl;
+            cout<<"Wybierz pozycje z menu: ";
+            cin>>x;
+
+            if((x>0)&&(x<=3)){
+                g1._game(x);
+            }
+        }while(x!=0);
+
+    cout<<"Koniec gry!"<<endl;
+
+    system("pause");
     return 0;
 }
